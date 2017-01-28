c9abfd75bcc2747931242e32bcbdcaf70516bf28
Donnacha Oisín Kidney <mail@doisinkidney.com>
2017-01-28 13:50:53 +0000

    Basic soln

--- scrabble.py
+++ scrabble.py
@@ -33,3 +33,20 @@ for i in range(2,13):
     with open('words/%i-letter-words.json' % i) as file:
         j = json.loads(''.join(file))
         words.update(d['word'] for d in j)
+
+def score(word):
+    return sum(map(scores.__getitem__, word))
+
+from itertools import permutations
+
+def all_words(letters):
+    return (''.join(p) for i in range(1, len(letters)+1) for p in permutations(letters, i))
+
+def best(letters):
+    best_score, best_word = 0, None
+    for word in filter(words.__contains__, all_words(letters)):
+        s = score(word)
+        if s > best_score:
+            best_score, best_word = s, word
+            print(best_word)
+    return best_word
